2dc42d369ea7d264191d7a86740b990d717d50ae
Vũ Văn Vĩnh <51917244+r1azk@users.noreply.github.com>
2020-06-19 17:00:30 +0700

    Create chu so lon nhat.cpp

---
+++ chu so lon nhat.cpp
@@ -0,0 +1,16 @@
+#include"stdio.h"
+
+
+int main(){
+    int k,n,max;
+    printf("Nhap n = ");
+    scanf("%d",&n);
+    while(n){
+        k=n%10;
+        if(k>max){
+            max=k;
+        }
+        n/=10;
+    }
+    printf("Chu so lon nhat la : %d\n",max);
+}
