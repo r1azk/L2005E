2af267fa6bebe494841666f3b37b7507200181ed
Vũ Văn Vĩnh <51917244+r1azk@users.noreply.github.com>
2020-06-26 19:21:12 +0700

    Create so chan cuoi cung.cpp

---
+++ so chan cuoi cung.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+void nhapmang(int a[], int &n){ 
+	printf("nhap so phan tu :" );
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){ 
+ 		printf(" phan tu thu %d :",i);
+     	scanf("%d",&a[i]);
+   }
+}
+void sochancuoicung(int a[],int n){
+ 	int x;
+ 	printf("so chan cuoi cung : ");
+ 	for (int i=0;i<n;i++){
+ 		if (a[i]%2==0)x=a[i];
+ 		else{
+ 			printf("No EVEN number");
+		}
+	}
+	printf("%d",x);
+}
+int main(){
+  int a[50],n;
+  nhapmang(a,n);
+  sochancuoicung(a,n);
+  return 0;
+}
